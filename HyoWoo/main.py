3610c1f89cd983e2338632e4e0a96f7e1bed1d9e
Minve1y <150213364+Minve1y@users.noreply.github.com>
2023-11-29 14:09:46 +0900

    plane.png

--- HyoWoo/main.py
+++ HyoWoo/main.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode(size)
 done= False
 clock= pygame.time.Clock()
  
-airplane = pygame.image.load('https://github.com/kanglossom/includePrac/blob/e220941254fb6e03093249152ba009c0e8b20709/HyoWoo/plane.png')
+airplane = pygame.image.load('plane.png')
 airplane = pygame.transform.scale(airplane, (60, 45))
  
 def runGame():
